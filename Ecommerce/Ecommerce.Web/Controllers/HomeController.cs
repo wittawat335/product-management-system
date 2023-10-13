bcf5b4812f4f150f5c52e13d99537c57bd4e96ab
Wittawat Fuangfu <wittawat335@gmail.com>
2023-10-13 13:11:32 +0700

    #

--- Ecommerce/Ecommerce.Web/Controllers/HomeController.cs
+++ Ecommerce/Ecommerce.Web/Controllers/HomeController.cs
@@ -5,10 +5,17 @@ namespace Ecommerce.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult CheckEnvironment()
+        {
+            return Content("Environment : " + _environment.EnvironmentName);
         }
 
         public IActionResult Index()
