c00cf5616d6ce6f5fbbac3db78f6d5f8aad45dff
Wittawat Fuangfu <wittawat335@gmail.com>
2023-11-22 10:39:23 +0700

    #

--- Ecommerce/Ecommerce.Infrastructure/obj/Debug/net7.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ Ecommerce/Ecommerce.Infrastructure/obj/Debug/net7.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b7e2459f9c03ac89a2d8f849ff315011987910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dea6a6c2ee4032ba568877469ce2a2525258b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
